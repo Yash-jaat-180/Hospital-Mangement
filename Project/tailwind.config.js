2d06e8e1c20de432993670a4f0f6ba3ed99b8edd
Yash <yashsaharan18024@gmail.com>
2024-10-20 18:23:23 +0530

    added the functionality of login and appointment booking in the local storage

--- Project/tailwind.config.js
+++ Project/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.html"],
+  
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+    },
   },
   plugins: [],
 }
